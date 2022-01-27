13f4bf601adb292a80c6254b776de34ea7322644
aloysius <alloystory@gmail.com>
2022-01-28 06:03:05 +0800

    move hardhat into a separate package

---
+++ packages/hardhat/contracts/TZFEToken.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract TZFEToken is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("TZFEToken", "TZFE") {}
+
+    function mint(address player, string memory tokenURI)
+        external
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
