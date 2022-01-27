9cbc5e2fdcbddd396b8874b0e396344452236463
aloysius <alloystory@gmail.com>
2022-01-28 07:11:09 +0800

    add connect to home page

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,8 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
+import { defaultChains, Provider as WagmiProvider } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Set up connectors
+  const connectors = () => {
+    return [new InjectedConnector({ chains: defaultChains })]
+  }
+
+  return (
+    <WagmiProvider {...{ autoConnect: true, connectors }}>
+      <Component {...pageProps} />
+    </WagmiProvider>
+  )
 }
 
 export default MyApp
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import { useAccount, useConnect } from 'wagmi'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [{ data: connectData, error: connectError }, connect] = useConnect()
+  const [{ data: accountData }, disconnect] = useAccount({
+    fetchEns: true,
+  })
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +18,37 @@ const Home: NextPage = () => {
 
       <main className="container">
         <h1>Welcome to Arcadium</h1>
-        <button className="btn btn-primary">Connect</button>
+        {accountData ? (
+          <div>
+            {accountData.ens?.avatar && (
+              <Image src={accountData.ens.avatar} alt="ENS Avatar" />
+            )}
+            <div>
+              {accountData.ens?.name
+                ? `${accountData.ens.name} (${accountData.address})`
+                : accountData.address}
+            </div>
+            <div>Connected to {accountData.connector?.name}</div>
+            <button className="btn btn-primary" onClick={disconnect}>
+              Disconnect
+            </button>
+          </div>
+        ) : (
+          connectData.connectors.map((connector) => (
+            <button
+              key={connector.id}
+              className="btn btn-primary"
+              disabled={!connector.ready}
+              onClick={() => connect(connector)}
+            >
+              Connect via {connector.name}
+              {!connector.ready && ' (unsupported)'}
+            </button>
+          ))
+        )}
+        {connectError && (
+          <div>{connectError?.message ?? 'Failed to connect'}</div>
+        )}
       </main>
     </div>
   )
